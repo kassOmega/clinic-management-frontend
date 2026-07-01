c7fc0d4326953efe805031f6bcbad8c576754330
Kassahun Takele <kass3me@gmail.com>
2026-07-01 21:25:27 +0300

    added some missed icons

--- src/components/icons.tsx
+++ src/components/icons.tsx
@@ -279,6 +279,27 @@ export function IconEye({ className = "w-5 h-5" }: { className?: string }) {
     </svg>
   );
 }
+export function IconEyeSlash({
+  className = "w-5 h-5",
+}: {
+  className?: string;
+}) {
+  return (
+    <svg
+      className={className}
+      fill="none"
+      viewBox="0 0 24 24"
+      stroke="currentColor"
+      strokeWidth={1.5}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3 3 3m12-12 3 3-3 3"
+      />
+    </svg>
+  );
+}
 export function IconSearch({ className = "w-5 h-5" }: { className?: string }) {
   return (
     <svg
